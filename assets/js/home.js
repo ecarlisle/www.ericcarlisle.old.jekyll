651985b90cc7f5823ab2dce935785518ac8f9aaa
Eric Carlisle <eric@ericcarlisle.com>
2015-03-25 10:48:52 -0400

    Move icon files. Refactor homepage JS into separate file.

---
+++ assets/js/home.js
@@ -0,0 +1,20 @@
+
+
+
+
+$(document).ready(function(){
+
+	// Grab content for homepage.
+	$.get( "/data/home.json", function(data) {
+
+		// Populate presentation content.
+	  var presentations = Mustache.render($('#presentation-list-item').html(), data);
+	  $('#presentation-list').html(presentations);
+
+		// Populate social content.
+	  //var social = Mustache.render($('#social-icons').html(), data);
+	  //$('#social-icon').html(social);
+
+	});
+
+});
